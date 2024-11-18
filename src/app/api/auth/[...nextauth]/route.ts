009e7c94c0b2f192a71baa604f4d023848c6491b
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-11-18 16:20:44 +0400

    configure nextauth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,23 @@
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import NextAuth from "next-auth"
+import EmailProvider from "next-auth/providers/email"
+import prisma from "@/lib/prisma"
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    })
+  ]
+})
+
+export { handler as GET, handler as POST }
