35ee1eec4bafbd9a55368ef044cb3cacc7475aa9
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-11-18 17:40:53 +0400

    change provider to credentialprovider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,6 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
-import EmailProvider from "next-auth/providers/email";
+import CredentialsProvider from "next-auth/providers/credentials";
 import prisma from "./prisma";
 
 export const authOptions: NextAuthOptions = {
@@ -8,24 +8,23 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt"
   },
-  providers: [
-    EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD
-        },
-      },
-      from: process.env.EMAIL_FROM,
-    })
-  ],
   pages: {
     signIn: "/auth/signin",
     // signOut: "/auth/signout",
     // error: "/auth/error",
     // verifyRequest: "/auth/verify-request",
     newUser: "/auth/register"
-  }
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {},
+        password: {}
+      },
+      async authorize(credentials, req) {
+        return null;
+      }
+    })
+  ]
 }
