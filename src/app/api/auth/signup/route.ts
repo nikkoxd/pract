9176fcbcb00e604f49b8394a146eb53ffb8d115a
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-11-18 18:41:19 +0400

    create a new user on post

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,10 +1,22 @@
 import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import prisma from "@/lib/prisma";
 
 export async function POST(request: Request) {
   try {
     const { email, password } = await request.json();
 
     console.log({ email, password });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const response = await prisma.user.create({
+      data: {
+        email: email,
+        password: hashedPassword,
+        role: "TEACHER",
+      },
+    });
   } catch (e) {
     console.log({ e });
   }
