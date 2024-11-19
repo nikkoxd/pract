aaf88bd3ab4a4928558b51b2d3dbf4b8f48ed89b
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-11-19 15:16:07 +0400

    add main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+import TableCell from "./components/TableCell";
+import TableHeader from "./components/TableHeader";
+import Sidebar from "./sidebar";
+
 export default function Home() {
   return (
-    <div>
-      123
-    </div>
+    <main className="container mx-auto grid grid-cols-4 gap-4">
+      <Sidebar />
+      <section className="col-span-3 bg-gray-100 dark:bg-gray-800 p-5 rounded-lg">
+        <h1 className="text-2xl font-bold">Базы данных - Б22-191-1</h1>
+        <table className="w-full table-auto">
+          <thead>
+            <tr>
+              <TableHeader>ФИО Участника</TableHeader>
+              <TableHeader>1</TableHeader>
+              <TableHeader>2</TableHeader>
+              <TableHeader>3</TableHeader>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <TableCell>Иванов Иван Иванович</TableCell>
+              <TableCell>+</TableCell>
+              <TableCell>+</TableCell>
+              <TableCell>+</TableCell>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    </main>
   );
 }
